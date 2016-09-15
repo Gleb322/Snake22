512b22668c940f21b605caa505c66b1666d9f04b
Gleb322 <glebka.smirnov.2005@mail.ru>
2016-09-15 23:18:50 +0400

    Lines

--- Snake32232/Program.cs
+++ Snake32232/Program.cs
@@ -11,40 +11,24 @@ namespace Snake32232
         {
             Point p1 = new Point(1,3,'*');
 
-
+            p1.Draw();
             
 
 
             Point p2 = new Point(4,5,'#');
-          
 
-            
-
-            Point p3 = new Point(6, 7, '@');
-
-           
-
-            Point p4 = new Point(8, 2, '&');
-
-            
+            p2.Draw();
 
 
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
 
 
-            
 
-            foreach(Point i in pList) 
-            {
-                i.Draw();
-            }
+            HorizontalLine line = new HorizontalLine(5, 10, 8,'+');
+            line.Drow();
 
-            
 
+            VerticalLine line1 = new VerticalLine(5,10, 25, '+');
+            line1.Drow();
 
             Console.ReadLine();
 
