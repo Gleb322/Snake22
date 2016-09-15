59450a247abd55448f320834580e0f5536f9bcec
Gleb322 <glebka.smirnov.2005@mail.ru>
2016-09-15 19:10:23 +0400

    Metods.

--- Snake32232/Program.cs
+++ Snake32232/Program.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace snake
+namespace Snake32232
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
 
-            Draw(x1, y1, sym1);
+            p1.Draw();
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
 
-            Draw(x2, y2, sym2);
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+
+            p2.Draw();
 
 
 
@@ -27,11 +30,8 @@ namespace snake
 
         }
 
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
+       
+    
     }
 }
   
