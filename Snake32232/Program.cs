5e26259b0a6e0f9df684ea05c389a37fdb3d23c1
Gleb322 <glebka.smirnov.2005@mail.ru>
2016-09-15 22:28:16 +0400

    List

--- Snake32232/Program.cs
+++ Snake32232/Program.cs
@@ -9,21 +9,41 @@ namespace Snake32232
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
-
-            p1.Draw();
+            Point p1 = new Point(1,3,'*');
 
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            
 
-            p2.Draw();
 
+            Point p2 = new Point(4,5,'#');
+          
+
+            
+
+            Point p3 = new Point(6, 7, '@');
+
+           
+
+            Point p4 = new Point(8, 2, '&');
+
+            
+
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+
+            
+
+            foreach(Point i in pList) 
+            {
+                i.Draw();
+            }
+
+            
 
 
             Console.ReadLine();
